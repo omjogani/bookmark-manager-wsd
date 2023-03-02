f13e2fc63301b4d0423fb9dc761df2bcc582087e
Om Jogani <72139914+omjogani@users.noreply.github.com>
2023-03-02 14:32:12 +0000

    [Added] : Dashboard and Components

--- bookmarkmanager/src/App.js
+++ bookmarkmanager/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import Welcome from './Components/Welcome';
+import Sidebar from './Components/Dashboard/Sidebar';
+import MainDash from './Components/Dashboard/MainDash/MainDash';
+import RightSide from './Components/Dashboard/RigtSide/RightSide';
+
 
 function App() {
   return (
     <div className="App">
-      <Welcome/>
+      {/* <Welcome/> */}
+      {/* <Dashboard/> */}
+      <div className="AppGlass">
+        <Sidebar/>
+        <MainDash/>
+        <RightSide/>
+      </div>
     </div>
   );
 }
